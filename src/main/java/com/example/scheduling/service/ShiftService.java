ab934415db5076d9716fd8acab9ee347101bd99e
Jasmine Wang <daisz@live.co.uk>
2025-07-30 17:38:18 -0400

    did input validation partially for shiftservice's create shift...will test out other flow in main

--- src/main/java/com/example/scheduling/service/ShiftService.java
+++ src/main/java/com/example/scheduling/service/ShiftService.java
@@ -1,5 +1,7 @@
 package com.example.scheduling.service;
 
+import java.time.LocalDateTime;
+
 import com.example.scheduling.model.Shift;
 import com.example.scheduling.repository.ShiftRepository;
 
@@ -12,7 +14,13 @@ public class ShiftService {
         this.shiftRepository = shiftRepository;
     }
         public Shift createShift(Shift shift) {
-            return shift;
+            //convert shiftStartDate+shiftStartTime and enddate+endttime into flattened localDatetIME OBJECT 
+
+            LocalDateTime startDateTime = LocalDatetime.of(shift.getShiftStartDate(), shift.getShiftStartTime());
+            
+
+    
+            return shift
     
 }
 }
